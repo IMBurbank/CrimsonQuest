eba3c500d9fa3449aebe642ea0b4044483c43422
IMBurbank <bassmanburbank@gmail.com>
2017-03-29 01:38:39 -0600

    Initial room creation work

--- app/js/main.es6
+++ app/js/main.es6
@@ -1,109 +1,222 @@
+const roomSize = {
+  '2': { min: 15, max: 29 },
+  '3': { min: 12, max: 18 },
+  '4': { min: 9, max: 13 }
+};
 
+/**
+  * Helper Functions
+  */
+
+const randInt = function randomIntFromRange(min, max) {
+  return Math.floor(min + Math.random() * (max - min + 1));
+}
+
+const newRoomOne = function createNewRoomOne (rows, cols) {
+  //Subtract two to leave room for permimeter walls
+  const xMin = roomSize[cols].min - 2,
+    xMax = roomSize[cols].max - 2,
+    yMin = roomSize[rows].min - 2,
+    yMax = roomSize[rows].max - 2,
+    w = randInt(xMin, xMax),
+    h = randInt(yMin, yMax),
+    xOffset = randInt(0, xMax - w),
+    yOffset = randInt(0, yMax - h),
+    roomFloorArr = [];
+
+  let i = 0,
+    j = 0;
+
+  roomFloorArr.length = yMax;
+  for (; i < yMax; i++) {
+    roomFloorArr[i] = [];
+    for (; j < xMax; j++) {
+      if (j + 1 > xOffSet && j + 1 < xOffset + w && i + 1 > yOffset && i + 1 < yOffset + h) {
+        roomFloorArr[i][j] = 1;
+      } else {
+        roomFloorArr[i][j] = 0;
+      }
+    }
+  }
+
+  return {
+    xOffset,
+    yOffset,
+    roomFloorArr,
+    areaWidth: xMax,
+    areaHeight: yMax,
+    floorWidth: w,
+    floorHeight: h
+  };
+
+}
+
+
+/**
+  * React Components
+  */
 
 class GameLevel extends React.Component {
-	render() {
-		return (
-			<div className='level'>
-				Game Level
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='level'>
+        Game Level
+      </div>
+    );
+  }
 }
 
 class CharacterInfo extends React.Component {
-	render() {
-		return (
-			<div className='char-info'>
-				Character Info
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='char-info'>
+        Character Info
+      </div>
+    );
+  }
 }
 
 class GameItems extends React.Component {
-	render() {
-		return (
-			<div className='items'>
-				Game Items
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='items'>
+        Game Items
+      </div>
+    );
+  }
 }
 
 class EnemiesRemaining extends React.Component {
-	render() {
-		return (
-			<div className='enemies-remaining'>
-				Enemies Remaining
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='enemies-remaining'>
+        Enemies Remaining
+      </div>
+    );
+  }
 }
 
 class EnemyStats extends React.Component {
-	render() {
-		return (
-			<div className='enemy-stats'>
-				Enemy Stats
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='enemy-stats'>
+        Enemy Stats
+      </div>
+    );
+  }
 }
 
 class ActivityLog extends React.Component {
-	render() {
-		return (
-			<div className='activity-log'>
-				Activity Log
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='activity-log'>
+        Activity Log
+      </div>
+    );
+  }
 }
 
 class GameTips extends React.Component {
-	render() {
-		return (
-			<div className='tips'>
-				Game Tips
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='tips'>
+        Game Tips
+      </div>
+    );
+  }
+}
+
+class BackgroundLayer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.createRooms = this.createRooms.bind(this);
+  }
+
+  createRooms() {
+    let rows = 0,
+      cols = [],
+      rooms = [],
+      rmsRow = [],
+      i = 0,
+      j = 0;
+
+    rows = randInt(3, 4);
+    cols.length = rows;
+    for (; i < rows; i++) {
+      cols[i] = randInt(2, 4);
+    }
+    console.log('rows: ' + rows, 'cols: ' + cols);
+
+    rooms = cols.map((el, index) => {
+      rmsRow = [];
+
+      for (j = 0; j < el; j++) {
+        rmsRow[j] = {
+          rows,
+          columns: el,
+          roomType: randInt(1, 3)
+        };
+      }
+
+      return rmsRow;
+    });
+
+    console.log('rooms: ', rooms);
+
+
+
+  }
+
+  render() {
+    this.createRooms();
+    return (
+      <div></div>
+    );
+  }
 }
 
 class GameStage extends React.Component {
-	render() {
-		return (
-			<div className='stage'>
-				Game Stage
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div className='stage'>
+        <BackgroundLayer/> {/*<AccentLayer	/>*/}
+        {/*<ItemLayer	/>*/}
+        {/*<EnemyLayer	/>*/}
+        {/*<FogLayer	/>*/}
+        {/*<PlayerLayer	/>*/}
+      </div>
+    );
+  }
 }
 
 class Game extends React.Component {
-	render() {
-		return (
-			<div className='game'>
-				<div className='col-lft'>
-					<GameLevel	/>
-					<CharacterInfo	/>
-				</div>
-				<div className='col-mid'>
-					<div className='title'>CrimsonQuest</div>
-					<GameStage	/>
-					<GameItems	/>
-				</div>
-				<div className='col-rgt'>
-					<EnemiesRemaining	/>
-					<EnemyStats	/>
-					<ActivityLog	/>
-					<GameTips	/>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='game'>
+        <div className='col-lft'>
+          <GameLevel/>
+          <CharacterInfo/>
+        </div>
+        <div className='col-mid'>
+          <div className='title'>CrimsonQuest</div>
+          <GameStage/>
+          <GameItems/>
+        </div>
+        <div className='col-rgt'>
+          <EnemiesRemaining/>
+          <EnemyStats/>
+          <ActivityLog/>
+          <GameTips/>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 /**
 	*		Static Page Components
@@ -114,16 +227,16 @@ class Game extends React.Component {
 	*		@returns {HTML} page header
 	*/
 class PageHeader extends React.Component {
-	shouldComponentUpdate() {
-		return false;
-	}
-	render() {
-		return (
-			<div className='pg-header'>
-				<h1>CrimsonQuest</h1>
-			</div>
-		);
-	}
+  shouldComponentUpdate() {
+    return false;
+  }
+  render() {
+    return (
+      <div className='pg-header'>
+        <h1>CrimsonQuest</h1>
+      </div>
+    );
+  }
 }
 
 /**
@@ -131,23 +244,23 @@ class PageHeader extends React.Component {
 	*		@returns {HTML} page header
 	*/
 class PageFooter extends React.Component {
-	shouldComponentUpdate() {
-		return false;
-	}
-	render() {
-		return (
-			<div className='pg-footer'>
-				<span>© 2017 Isaac Burbank.</span>
-				<span>
-					Tiles By&nbsp;
-					<a href='http://opengameart.org/content/dawnlike-16x16-universal-rogue-like-tileset-v181' target='_blank'>
-					DragonDePlatino </a>
-				</span>
-			</div>
-		);
-	}
+  shouldComponentUpdate() {
+    return false;
+  }
+  render() {
+    return (
+      <div className='pg-footer'>
+        <span>© 2017 Isaac Burbank.</span>
+        <span>
+          Tiles By&nbsp;
+          <a href='http://opengameart.org/content/dawnlike-16x16-universal-rogue-like-tileset-v181' target='_blank'>
+            DragonDePlatino
+          </a>
+        </span>
+      </div>
+    );
+  }
 }
-
 
 /**
 	*		Full App Class
@@ -158,19 +271,18 @@ class PageFooter extends React.Component {
 	*		@returns {HTML} full app
 	*/
 class App extends React.Component {
-	render() {
-		return (
-			<div className='pg'>
-				<PageHeader	/>
-				<div className='pg-content'>
-					<Game	/>
-				</div>
-				<PageFooter	/>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='pg'>
+        <PageHeader/>
+        <div className='pg-content'>
+          <Game/>
+        </div>
+        <PageFooter/>
+      </div>
+    );
+  }
 }
-
 
 /**
 	*		Render App to DOM
@@ -180,7 +292,5 @@ class App extends React.Component {
 	*		@desc ReactDOM renders app to HTML root node
 	*		@returns {DOM} full page
 	*/
-ReactDOM.render (
-	<App	/>,
-	document.getElementById('root')
-)
+ReactDOM.render(
+  <App/>, document.getElementById('root'))
--- dist/js/main.min.js
+++ dist/js/main.min.js
@@ -8,235 +8,348 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var roomSize = {
+  '2': { min: 15, max: 29 },
+  '3': { min: 12, max: 18 },
+  '4': { min: 9, max: 13 }
+};
+
+/**
+  * Helper Functions
+  */
+
+var randInt = function randomIntFromRange(min, max) {
+  return Math.floor(min + Math.random() * (max - min + 1));
+};
+
+var newRoomOne = function createNewRoomOne(rows, cols) {
+  //Subtract two to leave room for permimeter walls
+  var xMin = roomSize[cols].min - 2,
+      xMax = roomSize[cols].max - 2,
+      yMin = roomSize[rows].min - 2,
+      yMax = roomSize[rows].max - 2,
+      w = randInt(xMin, xMax),
+      h = randInt(yMin, yMax),
+      xOffset = randInt(0, xMax - w),
+      yOffset = randInt(0, yMax - h),
+      roomFloorArr = [];
+
+  var i = 0,
+      j = 0;
+
+  roomFloorArr.length = yMax;
+  for (; i < yMax; i++) {
+    roomFloorArr[i] = [];
+    for (; j < xMax; j++) {
+      if (j + 1 > xOffSet && j + 1 < xOffset + w && i + 1 > yOffset && i + 1 < yOffset + h) {
+        roomFloorArr[i][j] = 1;
+      } else {
+        roomFloorArr[i][j] = 0;
+      }
+    }
+  }
+
+  return {
+    xOffset: xOffset,
+    yOffset: yOffset,
+    roomFloorArr: roomFloorArr,
+    areaWidth: xMax,
+    areaHeight: yMax,
+    floorWidth: w,
+    floorHeight: h
+  };
+};
+
+/**
+  * React Components
+  */
+
 var GameLevel = function (_React$Component) {
-	_inherits(GameLevel, _React$Component);
+  _inherits(GameLevel, _React$Component);
 
-	function GameLevel() {
-		_classCallCheck(this, GameLevel);
+  function GameLevel() {
+    _classCallCheck(this, GameLevel);
 
-		return _possibleConstructorReturn(this, (GameLevel.__proto__ || Object.getPrototypeOf(GameLevel)).apply(this, arguments));
-	}
+    return _possibleConstructorReturn(this, (GameLevel.__proto__ || Object.getPrototypeOf(GameLevel)).apply(this, arguments));
+  }
 
-	_createClass(GameLevel, [{
-		key: 'render',
-		value: function render() {
-			return React.createElement(
-				'div',
-				{ className: 'level' },
-				'Game Level'
-			);
-		}
-	}]);
+  _createClass(GameLevel, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'level' },
+        'Game Level'
+      );
+    }
+  }]);
 
-	return GameLevel;
+  return GameLevel;
 }(React.Component);
 
 var CharacterInfo = function (_React$Component2) {
-	_inherits(CharacterInfo, _React$Component2);
+  _inherits(CharacterInfo, _React$Component2);
 
-	function CharacterInfo() {
-		_classCallCheck(this, CharacterInfo);
+  function CharacterInfo() {
+    _classCallCheck(this, CharacterInfo);
 
-		return _possibleConstructorReturn(this, (CharacterInfo.__proto__ || Object.getPrototypeOf(CharacterInfo)).apply(this, arguments));
-	}
+    return _possibleConstructorReturn(this, (CharacterInfo.__proto__ || Object.getPrototypeOf(CharacterInfo)).apply(this, arguments));
+  }
 
-	_createClass(CharacterInfo, [{
-		key: 'render',
-		value: function render() {
-			return React.createElement(
-				'div',
-				{ className: 'char-info' },
-				'Character Info'
-			);
-		}
-	}]);
+  _createClass(CharacterInfo, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'char-info' },
+        'Character Info'
+      );
+    }
+  }]);
 
-	return CharacterInfo;
+  return CharacterInfo;
 }(React.Component);
 
 var GameItems = function (_React$Component3) {
-	_inherits(GameItems, _React$Component3);
+  _inherits(GameItems, _React$Component3);
 
-	function GameItems() {
-		_classCallCheck(this, GameItems);
+  function GameItems() {
+    _classCallCheck(this, GameItems);
 
-		return _possibleConstructorReturn(this, (GameItems.__proto__ || Object.getPrototypeOf(GameItems)).apply(this, arguments));
-	}
+    return _possibleConstructorReturn(this, (GameItems.__proto__ || Object.getPrototypeOf(GameItems)).apply(this, arguments));
+  }
 
-	_createClass(GameItems, [{
-		key: 'render',
-		value: function render() {
-			return React.createElement(
-				'div',
-				{ className: 'items' },
-				'Game Items'
-			);
-		}
-	}]);
+  _createClass(GameItems, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'items' },
+        'Game Items'
+      );
+    }
+  }]);
 
-	return GameItems;
+  return GameItems;
 }(React.Component);
 
 var EnemiesRemaining = function (_React$Component4) {
-	_inherits(EnemiesRemaining, _React$Component4);
+  _inherits(EnemiesRemaining, _React$Component4);
 
-	function EnemiesRemaining() {
-		_classCallCheck(this, EnemiesRemaining);
+  function EnemiesRemaining() {
+    _classCallCheck(this, EnemiesRemaining);
 
-		return _possibleConstructorReturn(this, (EnemiesRemaining.__proto__ || Object.getPrototypeOf(EnemiesRemaining)).apply(this, arguments));
-	}
+    return _possibleConstructorReturn(this, (EnemiesRemaining.__proto__ || Object.getPrototypeOf(EnemiesRemaining)).apply(this, arguments));
+  }
 
-	_createClass(EnemiesRemaining, [{
-		key: 'render',
-		value: function render() {
-			return React.createElement(
-				'div',
-				{ className: 'enemies-remaining' },
-				'Enemies Remaining'
-			);
-		}
-	}]);
+  _createClass(EnemiesRemaining, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'enemies-remaining' },
+        'Enemies Remaining'
+      );
+    }
+  }]);
 
-	return EnemiesRemaining;
+  return EnemiesRemaining;
 }(React.Component);
 
 var EnemyStats = function (_React$Component5) {
-	_inherits(EnemyStats, _React$Component5);
+  _inherits(EnemyStats, _React$Component5);
 
-	function EnemyStats() {
-		_classCallCheck(this, EnemyStats);
+  function EnemyStats() {
+    _classCallCheck(this, EnemyStats);
 
-		return _possibleConstructorReturn(this, (EnemyStats.__proto__ || Object.getPrototypeOf(EnemyStats)).apply(this, arguments));
-	}
+    return _possibleConstructorReturn(this, (EnemyStats.__proto__ || Object.getPrototypeOf(EnemyStats)).apply(this, arguments));
+  }
 
-	_createClass(EnemyStats, [{
-		key: 'render',
-		value: function render() {
-			return React.createElement(
-				'div',
-				{ className: 'enemy-stats' },
-				'Enemy Stats'
-			);
-		}
-	}]);
+  _createClass(EnemyStats, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'enemy-stats' },
+        'Enemy Stats'
+      );
+    }
+  }]);
 
-	return EnemyStats;
+  return EnemyStats;
 }(React.Component);
 
 var ActivityLog = function (_React$Component6) {
-	_inherits(ActivityLog, _React$Component6);
+  _inherits(ActivityLog, _React$Component6);
 
-	function ActivityLog() {
-		_classCallCheck(this, ActivityLog);
+  function ActivityLog() {
+    _classCallCheck(this, ActivityLog);
 
-		return _possibleConstructorReturn(this, (ActivityLog.__proto__ || Object.getPrototypeOf(ActivityLog)).apply(this, arguments));
-	}
+    return _possibleConstructorReturn(this, (ActivityLog.__proto__ || Object.getPrototypeOf(ActivityLog)).apply(this, arguments));
+  }
 
-	_createClass(ActivityLog, [{
-		key: 'render',
-		value: function render() {
-			return React.createElement(
-				'div',
-				{ className: 'activity-log' },
-				'Activity Log'
-			);
-		}
-	}]);
+  _createClass(ActivityLog, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'activity-log' },
+        'Activity Log'
+      );
+    }
+  }]);
 
-	return ActivityLog;
+  return ActivityLog;
 }(React.Component);
 
 var GameTips = function (_React$Component7) {
-	_inherits(GameTips, _React$Component7);
+  _inherits(GameTips, _React$Component7);
 
-	function GameTips() {
-		_classCallCheck(this, GameTips);
+  function GameTips() {
+    _classCallCheck(this, GameTips);
 
-		return _possibleConstructorReturn(this, (GameTips.__proto__ || Object.getPrototypeOf(GameTips)).apply(this, arguments));
-	}
+    return _possibleConstructorReturn(this, (GameTips.__proto__ || Object.getPrototypeOf(GameTips)).apply(this, arguments));
+  }
 
-	_createClass(GameTips, [{
-		key: 'render',
-		value: function render() {
-			return React.createElement(
-				'div',
-				{ className: 'tips' },
-				'Game Tips'
-			);
-		}
-	}]);
+  _createClass(GameTips, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'tips' },
+        'Game Tips'
+      );
+    }
+  }]);
 
-	return GameTips;
+  return GameTips;
 }(React.Component);
 
-var GameStage = function (_React$Component8) {
-	_inherits(GameStage, _React$Component8);
+var BackgroundLayer = function (_React$Component8) {
+  _inherits(BackgroundLayer, _React$Component8);
 
-	function GameStage() {
-		_classCallCheck(this, GameStage);
+  function BackgroundLayer(props) {
+    _classCallCheck(this, BackgroundLayer);
 
-		return _possibleConstructorReturn(this, (GameStage.__proto__ || Object.getPrototypeOf(GameStage)).apply(this, arguments));
-	}
+    var _this8 = _possibleConstructorReturn(this, (BackgroundLayer.__proto__ || Object.getPrototypeOf(BackgroundLayer)).call(this, props));
 
-	_createClass(GameStage, [{
-		key: 'render',
-		value: function render() {
-			return React.createElement(
-				'div',
-				{ className: 'stage' },
-				'Game Stage'
-			);
-		}
-	}]);
+    _this8.createRooms = _this8.createRooms.bind(_this8);
+    return _this8;
+  }
 
-	return GameStage;
+  _createClass(BackgroundLayer, [{
+    key: 'createRooms',
+    value: function createRooms() {
+      var rows = 0,
+          cols = [],
+          rooms = [],
+          rmsRow = [],
+          i = 0,
+          j = 0;
+
+      rows = randInt(3, 4);
+      cols.length = rows;
+      for (; i < rows; i++) {
+        cols[i] = randInt(2, 4);
+      }
+      console.log('rows: ' + rows, 'cols: ' + cols);
+
+      rooms = cols.map(function (el, index) {
+        rmsRow = [];
+
+        for (j = 0; j < el; j++) {
+          rmsRow[j] = {
+            rows: rows,
+            columns: el,
+            roomType: randInt(1, 3)
+          };
+        }
+
+        return rmsRow;
+      });
+
+      console.log('rooms: ', rooms);
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      this.createRooms();
+      return React.createElement('div', null);
+    }
+  }]);
+
+  return BackgroundLayer;
 }(React.Component);
 
-var Game = function (_React$Component9) {
-	_inherits(Game, _React$Component9);
+var GameStage = function (_React$Component9) {
+  _inherits(GameStage, _React$Component9);
 
-	function Game() {
-		_classCallCheck(this, Game);
+  function GameStage(props) {
+    _classCallCheck(this, GameStage);
 
-		return _possibleConstructorReturn(this, (Game.__proto__ || Object.getPrototypeOf(Game)).apply(this, arguments));
-	}
+    return _possibleConstructorReturn(this, (GameStage.__proto__ || Object.getPrototypeOf(GameStage)).call(this, props));
+  }
 
-	_createClass(Game, [{
-		key: 'render',
-		value: function render() {
-			return React.createElement(
-				'div',
-				{ className: 'game' },
-				React.createElement(
-					'div',
-					{ className: 'col-lft' },
-					React.createElement(GameLevel, null),
-					React.createElement(CharacterInfo, null)
-				),
-				React.createElement(
-					'div',
-					{ className: 'col-mid' },
-					React.createElement(
-						'div',
-						{ className: 'title' },
-						'CrimsonQuest'
-					),
-					React.createElement(GameStage, null),
-					React.createElement(GameItems, null)
-				),
-				React.createElement(
-					'div',
-					{ className: 'col-rgt' },
-					React.createElement(EnemiesRemaining, null),
-					React.createElement(EnemyStats, null),
-					React.createElement(ActivityLog, null),
-					React.createElement(GameTips, null)
-				)
-			);
-		}
-	}]);
+  _createClass(GameStage, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'stage' },
+        React.createElement(BackgroundLayer, null),
+        ' '
+      );
+    }
+  }]);
 
-	return Game;
+  return GameStage;
+}(React.Component);
+
+var Game = function (_React$Component10) {
+  _inherits(Game, _React$Component10);
+
+  function Game() {
+    _classCallCheck(this, Game);
+
+    return _possibleConstructorReturn(this, (Game.__proto__ || Object.getPrototypeOf(Game)).apply(this, arguments));
+  }
+
+  _createClass(Game, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'game' },
+        React.createElement(
+          'div',
+          { className: 'col-lft' },
+          React.createElement(GameLevel, null),
+          React.createElement(CharacterInfo, null)
+        ),
+        React.createElement(
+          'div',
+          { className: 'col-mid' },
+          React.createElement(
+            'div',
+            { className: 'title' },
+            'CrimsonQuest'
+          ),
+          React.createElement(GameStage, null),
+          React.createElement(GameItems, null)
+        ),
+        React.createElement(
+          'div',
+          { className: 'col-rgt' },
+          React.createElement(EnemiesRemaining, null),
+          React.createElement(EnemyStats, null),
+          React.createElement(ActivityLog, null),
+          React.createElement(GameTips, null)
+        )
+      );
+    }
+  }]);
+
+  return Game;
 }(React.Component);
 
 /**
@@ -249,36 +362,36 @@ var Game = function (_React$Component9) {
 	*/
 
 
-var PageHeader = function (_React$Component10) {
-	_inherits(PageHeader, _React$Component10);
+var PageHeader = function (_React$Component11) {
+  _inherits(PageHeader, _React$Component11);
 
-	function PageHeader() {
-		_classCallCheck(this, PageHeader);
+  function PageHeader() {
+    _classCallCheck(this, PageHeader);
 
-		return _possibleConstructorReturn(this, (PageHeader.__proto__ || Object.getPrototypeOf(PageHeader)).apply(this, arguments));
-	}
+    return _possibleConstructorReturn(this, (PageHeader.__proto__ || Object.getPrototypeOf(PageHeader)).apply(this, arguments));
+  }
 
-	_createClass(PageHeader, [{
-		key: 'shouldComponentUpdate',
-		value: function shouldComponentUpdate() {
-			return false;
-		}
-	}, {
-		key: 'render',
-		value: function render() {
-			return React.createElement(
-				'div',
-				{ className: 'pg-header' },
-				React.createElement(
-					'h1',
-					null,
-					'CrimsonQuest'
-				)
-			);
-		}
-	}]);
+  _createClass(PageHeader, [{
+    key: 'shouldComponentUpdate',
+    value: function shouldComponentUpdate() {
+      return false;
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'pg-header' },
+        React.createElement(
+          'h1',
+          null,
+          'CrimsonQuest'
+        )
+      );
+    }
+  }]);
 
-	return PageHeader;
+  return PageHeader;
 }(React.Component);
 
 /**
@@ -287,46 +400,46 @@ var PageHeader = function (_React$Component10) {
 	*/
 
 
-var PageFooter = function (_React$Component11) {
-	_inherits(PageFooter, _React$Component11);
+var PageFooter = function (_React$Component12) {
+  _inherits(PageFooter, _React$Component12);
 
-	function PageFooter() {
-		_classCallCheck(this, PageFooter);
+  function PageFooter() {
+    _classCallCheck(this, PageFooter);
 
-		return _possibleConstructorReturn(this, (PageFooter.__proto__ || Object.getPrototypeOf(PageFooter)).apply(this, arguments));
-	}
+    return _possibleConstructorReturn(this, (PageFooter.__proto__ || Object.getPrototypeOf(PageFooter)).apply(this, arguments));
+  }
 
-	_createClass(PageFooter, [{
-		key: 'shouldComponentUpdate',
-		value: function shouldComponentUpdate() {
-			return false;
-		}
-	}, {
-		key: 'render',
-		value: function render() {
-			return React.createElement(
-				'div',
-				{ className: 'pg-footer' },
-				React.createElement(
-					'span',
-					null,
-					'\xA9 2017 Isaac Burbank.'
-				),
-				React.createElement(
-					'span',
-					null,
-					'Tiles By\xA0',
-					React.createElement(
-						'a',
-						{ href: 'http://opengameart.org/content/dawnlike-16x16-universal-rogue-like-tileset-v181', target: '_blank' },
-						'DragonDePlatino '
-					)
-				)
-			);
-		}
-	}]);
+  _createClass(PageFooter, [{
+    key: 'shouldComponentUpdate',
+    value: function shouldComponentUpdate() {
+      return false;
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'pg-footer' },
+        React.createElement(
+          'span',
+          null,
+          '\xA9 2017 Isaac Burbank.'
+        ),
+        React.createElement(
+          'span',
+          null,
+          'Tiles By\xA0',
+          React.createElement(
+            'a',
+            { href: 'http://opengameart.org/content/dawnlike-16x16-universal-rogue-like-tileset-v181', target: '_blank' },
+            'DragonDePlatino'
+          )
+        )
+      );
+    }
+  }]);
 
-	return PageFooter;
+  return PageFooter;
 }(React.Component);
 
 /**
@@ -339,33 +452,33 @@ var PageFooter = function (_React$Component11) {
 	*/
 
 
-var App = function (_React$Component12) {
-	_inherits(App, _React$Component12);
+var App = function (_React$Component13) {
+  _inherits(App, _React$Component13);
 
-	function App() {
-		_classCallCheck(this, App);
+  function App() {
+    _classCallCheck(this, App);
 
-		return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
-	}
+    return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+  }
 
-	_createClass(App, [{
-		key: 'render',
-		value: function render() {
-			return React.createElement(
-				'div',
-				{ className: 'pg' },
-				React.createElement(PageHeader, null),
-				React.createElement(
-					'div',
-					{ className: 'pg-content' },
-					React.createElement(Game, null)
-				),
-				React.createElement(PageFooter, null)
-			);
-		}
-	}]);
+  _createClass(App, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'pg' },
+        React.createElement(PageHeader, null),
+        React.createElement(
+          'div',
+          { className: 'pg-content' },
+          React.createElement(Game, null)
+        ),
+        React.createElement(PageFooter, null)
+      );
+    }
+  }]);
 
-	return App;
+  return App;
 }(React.Component);
 
 /**
